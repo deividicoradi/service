e798d36ce4640c1d6c8782f105fab144d6a6b645
deividicoradi <deividi.coradii@gmail.com>
2023-04-04 15:56:56 -0300

    Implementando Service Layer

---
+++ app/services/users/activate_service.rb
@@ -0,0 +1,18 @@
+module Users
+  class ActivateService
+    def initialize(user)
+      @user = user
+    end
+
+    def call(reason)
+      activate!(reason)
+    end
+
+    private
+    def activate!(reason)
+      @user.update(reason: reason, active: true)
+    end
+  end
+end
+
+# User::ActivateService.new(user).call
---
+++ app/services/users/deactivate_service.rb
@@ -0,0 +1,19 @@
+module Users
+  class DeactivateService
+    def initialize(user)
+      @user = user
+    end
+
+    def call(reason)
+      activate!(reason)
+    end
+
+    private
+    def activate!(reason)
+      @user.update(reason: reason, active: false)
+    end
+  end
+end
+
+# Users::ActivateService.new(user).call
+# Users::DeactivateService.new(user).call
